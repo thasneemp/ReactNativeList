b6dc213f94bf661d99f34cc0b6b9f326c67232bf
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2018-03-21 11:30:12 +0530

    final code

--- ReactList/screens/DetailsScreen.js
+++ ReactList/screens/DetailsScreen.js
@@ -14,8 +14,8 @@ export default class DetailsScreen extends React.Component {
     render() {
         return (
             <View>
-                <Text>{this.state.item.title}</Text>
-                <Text>{this.state.item.releaseYear}</Text>
+                <Text style={{backgroundColor:'blue', color:'white', padding:10}}>Name : {this.state.item.title}</Text>
+                <Text style={{backgroundColor:'#f4f4f5', color:'black', padding:10}}>Year : {this.state.item.releaseYear}</Text>
             </View>
         );
     }
